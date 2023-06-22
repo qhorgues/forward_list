122813ad10039cc74b06ec7e2f2f37ca6c8633c1
Quentin Horgues <quentin.horgues@outlook.fr>
2023-06-22 20:03:13 +0200

    remodelage et ajout de la fonction de tri

--- include/forward_list.h
+++ include/forward_list.h
@@ -11,264 +11,51 @@ extern "C"
 #include <stddef.h>
 #include <stdbool.h>
 
-struct forward_list_node_t;
-typedef struct forward_list_node_t forward_list_node_t;
-
-typedef struct forward_list_base_t {
-    forward_list_node_t* list;
-} forward_list_base_t;
-
+struct fw_list_t;
+typedef struct fw_list_t fw_list_t;
 /**
- * @brief forward_list_iter_t is pointeur to member of list
+ * @brief fw_list_iter_t is pointeur to member of list
  */
-typedef forward_list_node_t *forward_list_iter_t;
-
-
-typedef forward_list_node_t const *forward_list_citer_t;
-
-
-#define forward_list_t(Type)\
-    struct { \
-        forward_list_base_t base; \
-        Type *ref; \
-        Type const *cref; \
-        Type tmp; \
-    }
-
-
-/**************** Constructor / Destructor ******************/
-
-#define forward_list_init(forward_list)\
-    forward_list_init_(&(forward_list).base)
-
-#define forward_list_free(forward_list)\
-    forward_list_free_(&(forward_list).base)
-
-#define forward_list_is_init(forward_list)\
-    forward_list_is_init_(&(forward_list).base)
-
-
-/********************* Element access **********************/
-
-#define forward_list_front(forward_list)\
-    ( (forward_list)->ref = forward_list_front_(&(forward_list).base) )
-
-
-/************************ Iterators ************************/
-
-#define forward_list_cbefore_begin(forward_list)\
-    forward_list_cbefore_begin_(&(forward_list).base)
-
-#define forward_list_before_begin(forward_list)\
-    forward_list_before_begin_(&(forward_list).base)
-
-#define forward_list_cbegin(forward_list)\
-    forward_list_cbegin_(&(forward_list).base)
-
-#define forward_list_begin(forward_list)\
-    forward_list_begin_(&(forward_list).base)
-
-#define forward_list_cend()\
-    forward_list_cend_()
-
-#define forward_list_end()\
-    forward_list_end_()
-
-#define forward_list_cget(forward_list, const_iterator)\
-    ( forward_list.cref = forward_list_cget_(const_iterator) )
-
-#define forward_list_get(forward_list, iterator)\
-    ( forward_list.ref = forward_list_get_(iterator) )
-
-#define forward_list_set(forward_list, iterator, value) \
-    ( forward_list.tmp = value,\
-        forward_list_set_(iterator, forward_list.tmp, \
-                                  sizeof((forward_list)->tmp)) )
-
-
-/*********** Capacity ***********/
-
-#define forward_list_empty(forward_list)\
-    forward_list_empty_(&(forward_list).base)
-
-
-
-/********** Modifiers ***********/
-
-#define forward_list_clear(forward_list)\
-    forward_list_clear_(&(forward_list).base)
-
-#define forward_list_insert_after(forward_list, iterator, value)\
-    (forward_list.tmp = value, \
-        forward_list_insert_after_(iterator, &(forward_list).tmp, sizeof (value)))
-
-#define forward_list_erase_after(forward_list, iterator)\
-    forward_list_erase_after_(iterator)
-
-#define forward_list_push_front(forward_list, value)\
-    (forward_list.tmp = value, \
-        forward_list_push_front_(&(forward_list).base, &(forward_list).tmp, sizeof (value)))
-
-#define forward_list_pop_front(forward_list)\
-    forward_list_pop_front_(&(forward_list).base)
-
-
-/********** Operations ***********/
-
-#define forward_list_splice_after(forward_list_1, forward_list_2) \
-    forward_list_splice_after_(&(forward_list_1).base, &(forward_list_2).base)
-
-int forward_list_init_(forward_list_base_t *const restrict forward_list);
-
-void forward_list_free_(forward_list_base_t *const restrict forward_list);
-
-bool forward_list_is_init_(forward_list_base_t *const restrict forward_list);
-
-void* forward_list_front_(forward_list_base_t *const restrict forward_list);
-
-forward_list_citer_t forward_list_cbefore_begin_(forward_list_base_t *const restrict forward_list);
-
-forward_list_iter_t forward_list_before_begin_(forward_list_base_t *const restrict forward_list);
-
-forward_list_citer_t forward_list_cbegin_(forward_list_base_t *const restrict forward_list);
-
-forward_list_iter_t forward_list_begin_(forward_list_base_t *const restrict forward_list);
-
-forward_list_citer_t forward_list_cend_(void);
-
-forward_list_iter_t forward_list_end_(void);
-
-void const* forward_list_cget_(forward_list_citer_t const restrict iter);
-
-void* forward_list_get_(forward_list_iter_t const restrict iter);
-
-void forward_list_set_(forward_list_iter_t restrict iter,
-                                   void const *const restrict value, 
-                                   size_t const size_value);
-
-bool forward_list_empty_(forward_list_base_t *const restrict forward_list);
-
-void forward_list_clear_(forward_list_base_t *const restrict forward_list);
-
-forward_list_iter_t forward_list_insert_after_(forward_list_iter_t const restrict iter,
-                              void const *const restrict value, 
-                              size_t const size_value);
-
-int forward_list_erase_after_(forward_list_iter_t const restrict iter);
-
-forward_list_iter_t forward_list_push_front_(forward_list_base_t *const restrict forward_list, 
-                             void const *const value, 
-                             size_t const size_value);
-
-int forward_list_pop_front_(forward_list_base_t *const restrict forward_list);
-
-forward_list_citer_t forward_list_cnext(forward_list_citer_t iter);
-
-forward_list_iter_t forward_list_next(forward_list_iter_t iter);
-
-void forward_list_splice_after_(forward_list_base_t* restrict forward_list1, forward_list_base_t* restrict forward_list2);
-
-// Alias fw
-
-typedef forward_list_iter_t fw_list_iter_t;
-
-typedef forward_list_citer_t fw_list_citer_t;
-
-#define fw_list_t(Type)  \
-            forward_list_t(Type)
-
-#define fw_list_init(forward_list)  \
-            forward_list_init(forward_list)
-
-#define fw_list_free(forward_list)  \
-            forward_list_free(forward_list)
-
-#define fw_list_clear(forward_list)  \
-            forward_list_clear(forward_list)
-
-#define fw_list_get(forward_list, iterator)  \
-            forward_list_get(forward_list, iterator)
-
-#define fw_list_cget(forward_list, const_iterator)  \
-            forward_list_cget(forward_list, const_iterator)
-
-#define fw_list_set(forward_list, index, value)  \
-            forward_list_set(forward_list, index, value)
-
-#define fw_list_front(forward_list)  \
-            forward_list_front(forward_list)
-
-#define fw_list_insert_after(forward_list, iter, value)  \
-            forward_list_insert_after(forward_list, iter, value)
-
-#define fw_list_push_front(forward_list, value)  \
-            forward_list_push_front(forward_list, value)
-
-#define fw_list_erase_after(forward_list, iter)  \
-            forward_list_erase_after(forward_list, iter)
-
-#define fw_list_pop_front(forward_list)  \
-            forward_list_pop_front(forward_list)
-
-#define fw_list_begin(forward_list)  \
-            forward_list_begin(forward_list)
-
-#define fw_list_cbegin(forward_list)  \
-            forward_list_cbegin(forward_list)
-
-#define fw_list_before_begin(forward_list)  \
-            forward_list_before_begin(forward_list)
-
-#define fw_list_cbefore_begin(forward_list)  \
-            forward_list_cbefore_begin(forward_list)
-
-#define fw_list_end(forward_list)  \
-            forward_list_end(forward_list)
-
-#define fw_list_cend(forward_list)  \
-            forward_list_cend(forward_list)
-
-
-#define fw_list_cnext(iter)  \
-            forward_list_cnext(iter)
-
-#define fw_list_next(iter)  \
-            forward_list_next(iter)
-
-#define fw_list_splice_after(forward_list_1, forward_list_2) \
-    forward_list_splice_after(forward_list_1, forward_list_2)
-
-
-// default type
-
-// int
-typedef fw_list_t(int) forward_list_int_t;
-typedef fw_list_t(int) fw_list_int_t;
-
-// char
-typedef fw_list_t(char) forward_list_char_t;
-typedef fw_list_t(char) fw_list_char_t;
-
-// float
-typedef fw_list_t(float) forward_list_float_t;
-typedef fw_list_t(float) fw_list_float_t;
-
-// double
-typedef fw_list_t(double) forward_list_double_t;
-typedef fw_list_t(double) fw_list_double_t;
-
-// long long
-typedef fw_list_t(long long) forward_list_long_long_t;
-typedef fw_list_t(long long) fw_list_long_long_t;
-
-// char*
-typedef fw_list_t(char*) forward_list_str_t;
-typedef fw_list_t(char*) fw_list_str_t;
-
-// void*
-typedef fw_list_t(void*) forward_list_ptr_t;
-typedef fw_list_t(void*) fw_list_ptr_t;
-
+typedef fw_list_t *fw_list_iter_t;
+typedef fw_list_t const *fw_list_citer_t;
+
+void fw_list_free(fw_list_t *forward_list);
+void* fw_list_front(fw_list_t *forward_list);
+fw_list_citer_t fw_list_cbegin(fw_list_t *forward_list);
+fw_list_iter_t fw_list_begin(fw_list_t *forward_list);
+fw_list_citer_t fw_list_cend(void);
+fw_list_iter_t fw_list_end(void);
+void const* fw_list_cget(fw_list_citer_t iter);
+void* fw_list_get(fw_list_iter_t iter);
+void fw_list_set(fw_list_iter_t iter, void const *value, size_t size_value);
+bool fw_list_empty(fw_list_t const *forward_list);
+fw_list_iter_t fw_list_insert_after(fw_list_iter_t iter, void const *value, size_t size_value);
+int fw_list_erase_after(fw_list_iter_t iter);
+fw_list_t* fw_list_push_front(fw_list_t *forward_list, void const *value, size_t size_value);
+fw_list_t* fw_list_pop_front(fw_list_t *forward_list);
+fw_list_citer_t fw_list_cnext(fw_list_citer_t iter);
+fw_list_iter_t fw_list_next(fw_list_iter_t iter);
+fw_list_t* fw_list_splice_after(fw_list_t *forward_list1, fw_list_t *forward_list2);
+fw_list_t* fw_list_merge(fw_list_t *forward_list1, fw_list_t *forward_list2, int (*cmp)(const void*, const void*));
+fw_list_t* fw_list_sort(fw_list_t *forward_list, int (*cmp)(const void*, const void*));
+
+
+static inline fw_list_t* fw_list_init(void)
+{
+    return NULL;
+}
+
+static inline void fw_list_clear(fw_list_t **forward_list)
+{
+    fw_list_free(*forward_list);
+    *forward_list = NULL;
+}
+
+#define fw_cbefore_begin(forward_list) \
+    (fw_list_citer_t)(&forward_list)
+
+#define fw_list_before_begin(forward_list) \
+    (fw_list_iter_t)(&forward_list)
 
 #if defined(__cplusplus)
 }
--- test/main.c
+++ test/main.c
@@ -3,74 +3,36 @@
 #include <errno.h>
 #include <forward_list.h>
 
+int int_cmp(const void* a, const void* b)
+{
+    const int va = *(const int*)a;
+    const int vb = *(const int*)b;
+    
+    if (va < vb) return -1;
+    if (va > vb) return 1;
+    return 0;
+}
+
 int main(void)
 {
-    printf("\n list 1 \n\n");
-
-    fw_list_int_t list1;
-    fw_list_init(list1);
-    fw_list_push_front(list1, 12);
-    fw_list_push_front(list1, 24);
-
-    for (fw_list_citer_t iter = fw_list_cbegin(list1); iter != fw_list_cend(); iter = fw_list_cnext(iter))
+    fw_list_t *list1 = fw_list_init();
     {
-        int const* value = fw_list_cget(list1, iter); 
-        printf("%d, %p\n", *value, (void const*)value);
+        int x = 12;
+        list1 = fw_list_push_front(list1, &x, sizeof(x));
+        x = 32;
+        fw_list_iter_t iter = fw_list_begin(list1);
+        fw_list_insert_after(iter, &x, sizeof(x));
+        x = 24;
+        list1 = fw_list_push_front(list1, &x, sizeof(x));
+        x = 72;
+        list1 = fw_list_push_front(list1, &x, sizeof(x));
     }
 
-    printf("\n list 2 \n\n");
-
-    fw_list_int_t list2;
-    fw_list_init(list2);
-
-    fw_list_push_front(list2, 4);
-    fw_list_push_front(list2, 5);
-    fw_list_insert_after(list2, fw_list_begin(list2), 18);
-    fw_list_erase_after(list2, forward_list_before_begin(list2));
-    fw_list_pop_front(list2);
-    fw_list_push_front(list2, 25);
-    fw_list_push_front(list2, 52);
-
-    for (fw_list_citer_t iter2 = fw_list_cbegin(list2); iter2 != fw_list_cend(); iter2 = fw_list_cnext(iter2))
+    list1 = fw_list_sort(list1, &int_cmp);
+    for (fw_list_iter_t i = fw_list_begin(list1); i != fw_list_end(); i = fw_list_next(i))
     {
-        int const* value = fw_list_cget(list2, iter2);
-        printf("%d, %p\n", *value, (void const*)value);
-        
+        printf("%d\n", *(int*)fw_list_get(i));
     }
-
-    printf("\n list 1 + 2 \n\n");
-
-    fw_list_splice_after(list1, list2);
-    for (fw_list_citer_t iter = fw_list_cbegin(list1); iter != fw_list_cend(); iter = fw_list_cnext(iter))
-    {
-        int const* value = fw_list_cget(list1, iter); 
-        printf("%d, %p\n", *value, (void const*)value);
-    }
-
     fw_list_free(list1);
-
-    printf("\n list string \n\n");
-
-    char str[][10] = {"un", "deux", "trois", "quatre", "cinq", "six", "sept", "huit"};
-
-    fw_list_str_t list_str;
-    fw_list_init(list_str);
-
-    fw_list_iter_t list_str_iter = fw_list_before_begin(list_str);
-
-    for (int i = 0; i < 8; i++)
-    {
-        fw_list_insert_after(list_str, list_str_iter, str[i]);
-        list_str_iter = fw_list_next(list_str_iter);
-    }
-
-    for (fw_list_citer_t iter = fw_list_cbegin(list_str); iter != fw_list_cend(); iter = fw_list_cnext(iter))
-    {
-        char* const* value = fw_list_cget(list_str, iter); 
-        printf("%s, %p\n", *value, (void const*)value);
-    }
-
-    fw_list_free(list_str);
-
     return 0;
 }
